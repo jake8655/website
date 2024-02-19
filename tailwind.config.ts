5fdccf7be3185b77478e4f75a2824b854e30edc0
Jake <dominik8655@gmail.com>
2024-02-19 20:16:30 +0100

    feat(ui): add terminal

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,22 @@ export default {
           dark: '#005cb8',
         },
       },
+      keyframes: {
+        bounce: {
+          '0%, 100%': {
+            transform: 'none',
+            animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+          },
+          '50%': {
+            transform: 'translateY(-25%)',
+            animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+          },
+        },
+      },
+      boxShadow: {
+        glow: '0 0 50px 0 rgba(109, 154, 238, 0.3)',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide')],
 } satisfies Config;
