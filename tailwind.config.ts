3f420349e53f043d13e41d6f1c29ae351b6305d0
Jake <dominik8655@gmail.com>
2024-09-12 20:26:14 +0200

    feat: add shadcn/ui and aceternity/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,78 @@
 import type { Config } from "tailwindcss";
 import colors from "tailwindcss/colors";
+// @ts-expect-error this lib has no typedefs
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 import type { PluginCreator } from "tailwindcss/types/config";
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+// eslint-disable-next-line @typescript-eslint/unbound-method
+const addVariablesForColors: PluginCreator = ({ addBase, theme }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors as object).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+};
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      fontFamily: {
-        geist: ["Geist", "sans-serif"],
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
+    },
+    extend: {
       colors: {
-        primary: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        brand: {
           DEFAULT: "#6d9aee",
           light: "#b0c4de",
           dark: "#005cb8",
         },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
         bounce: {
@@ -35,10 +93,20 @@ export default {
           "0%, 100%": { color: "#b0c4de" },
           "50%": { color: colors.gray[900] },
         },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
         blink: "blink 1s infinite steps(1, start)",
         blinkText: "blinkText 1s infinite steps(1, start)",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       boxShadow: {
         glow: "0 0 50px 0 rgba(109, 154, 238, 0.3)",
@@ -48,5 +116,6 @@ export default {
   plugins: [
     require("tailwindcss-animate"),
     require("tailwind-scrollbar-hide"),
+    addVariablesForColors,
   ] as PluginCreator[],
 } satisfies Config;
