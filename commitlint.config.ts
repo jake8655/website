e83c47048c4c0168a22392ae54faa936c6ade1fe
Jake <dominik8655@gmail.com>
2024-12-06 23:02:45 +0100

    chore(commitlint): make body max line length infinity

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,9 @@ import type { UserConfig } from "@commitlint/types";
 
 const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [0, "always", Infinity],
+  },
 };
 
 export default config;
