af4d16e919b48bc84f04290cd1769e87aa65bdeb
Jake <dominik8655@gmail.com>
2025-01-02 21:59:54 +0100

    feat(projects): remove last placeholder and add hover animation

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -34,12 +34,14 @@ const projects = [
     backgroundColor: "bg-gradient-to-br from-teal-600 to-teal-800",
     href: "https://github.com/jake8655/gymgol-simulator",
     image: "/images/gymgol-simulator.png",
+    imagePosition: "bottom" as const,
     alt: "Gymgol Simulator",
     id: "gymgol-simulator",
   },
   {
     title: "Asteroids",
     backgroundColor: "bg-gradient-to-br from-slate-300 to-slate-400",
+    imagePosition: "left" as const,
     href: "https://github.com/jake8655/asteroids",
     image: "/images/asteroids.png",
     alt: "Asteroids Game",
@@ -56,9 +58,11 @@ export default function Projects({ className }: { className?: string }) {
           title={project.title}
           href={project.href}
           image={project.image}
+          imagePosition={project.imagePosition}
           alt={project.alt}
           id={project.id}
-          className={cn("project-card", project.backgroundColor)}
+          backGroundColor={project.backgroundColor}
+          className="project-card"
         />
       ))}
     </ProjectsSection>
@@ -69,49 +73,62 @@ async function ProjectCard({
   title,
   href,
   image,
+  imagePosition,
   alt,
   id,
   className,
+  backGroundColor,
 }: {
   title: string;
   href: string;
   image: string;
+  imagePosition?: "left" | "bottom";
   alt: string;
   id: string;
   className?: string;
+  backGroundColor: string;
 }) {
   await api.projectLike.getProjectLikeCount.prefetch({ projectId: id });
 
   return (
     <HydrateClient>
-      <a
-        className={cn(
-          "relative mb-[600px] block min-h-[600px] w-full rounded-[30px]",
-          className,
-        )}
+      <div
+        className={cn("mb-[600px] flex flex-col gap-4", className)}
         style={{
           transformOrigin: "50% -160%",
         }}
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
       >
-        <Image
-          src={image}
-          alt={alt}
-          fill
-          className="absolute inset-0 rounded-[30px] object-cover p-4"
-        />
-
-        <div className="project-text relative h-full w-full">
-          <h3 className="absolute bottom-0 left-0 translate-y-full pt-4 pl-4 font-bold text-xl">
-            {title}
-          </h3>
-          <div className="absolute right-0 bottom-0 flex translate-y-full items-center gap-8 pt-4 pr-4 font-bold text-lg">
+        <a
+          className={cn(
+            "group block min-h-[400px] w-full rounded-[46px] p-4 sm:min-h-[600px]",
+            backGroundColor,
+          )}
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="relative h-full w-full overflow-hidden rounded-[30px]">
+            <Image
+              src={image}
+              alt={alt}
+              fill
+              className={cn(
+                "rounded-[30px] object-cover transition-transform duration-300 group-hover:scale-105",
+                {
+                  "object-left": imagePosition === "left",
+                  "object-bottom": imagePosition === "bottom",
+                },
+              )}
+            />
+          </div>
+        </a>
+        <div className="project-text flex h-full">
+          <h3 className="pl-4 font-bold text-xl">{title}</h3>
+          <div className="ml-auto flex items-center gap-8 pr-4 font-bold text-lg">
             <ProjectLikes projectId={id} />
           </div>
         </div>
-      </a>
+      </div>
     </HydrateClient>
   );
 }
