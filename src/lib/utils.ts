745455233930b7b85333fd7c0cc70e923c56d5a9
Jake <dominik8655@gmail.com>
2025-01-06 15:03:47 +0100

    feat(contact): add client max char values in form
    to avoid misuse

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -20,10 +20,23 @@ export function msToTime(time: number) {
 }
 
 export const contactFormSchema = z.object({
-  name: z.string().min(1, "Name is required"),
-  email: z.string().email("Invalid email"),
-  subject: z.string().min(1, "Subject is required"),
-  message: z.string().min(1, "Message is required"),
+  name: z
+    .string()
+    .min(1, "Name is required")
+    .max(50, "Name must be 50 characters or less"),
+  email: z
+    .string()
+    .min(1, "Email is required")
+    .email("Invalid email")
+    .max(100, "Email must be 100 characters or less"),
+  subject: z
+    .string()
+    .min(1, "Subject is required")
+    .max(200, "Subject must be 200 characters or less"),
+  message: z
+    .string()
+    .min(1, "Message is required")
+    .max(1000, "Message must be 1000 characters or less"),
 });
 
 export type ContactFormSchema = z.infer<typeof contactFormSchema>;
