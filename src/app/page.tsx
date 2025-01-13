64043bd9c8f425b0c6b2a5c3c329359bce214e41
Jake <dominik8655@gmail.com>
2025-01-13 17:58:59 +0100

    fix(home): remove duplicate content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,48 +68,11 @@ export default function Home() {
           <div className="mt-64 grid place-items-center">
             <ArrowTitle text="My Projects" slideDirection="top" id="projects" />
           </div>
-          <Navbar />
           <Wrapper>
-            <Hero className="pt-16 md:pt-32 lg:pt-48" />
+            <Projects className="mt-32 w-full" />
           </Wrapper>
-          <RevealOnScroll
-            className="grid place-items-center"
-            variants={{
-              hidden: {
-                opacity: 0,
-                y: "-100%",
-              },
-              visible: {
-                opacity: 1,
-                y: 0,
-              },
-            }}
-            transition={{
-              delay: 0.6,
-            }}
-          >
-            <ArrowTitle
-              text="My Experience"
-              slideDirection="bottom"
-              key="experience"
-              id="experience"
-            />
-          </RevealOnScroll>
-          <Experience />
-          <div className="relative">
-            <div className="mt-64 grid place-items-center">
-              <ArrowTitle
-                text="My Projects"
-                slideDirection="top"
-                id="projects"
-              />
-            </div>
-            <Wrapper>
-              <Projects className="mt-32 w-full" />
-            </Wrapper>
-            <ShootingStars className="-z-10" />
-            <StarsBackground className="-z-10" />
-          </div>
+          <ShootingStars className="-z-10" />
+          <StarsBackground className="-z-10" />
         </div>
       </div>
       {/* Empty div to make the footer visible at the bottom */}
