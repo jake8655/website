10086a6f9eb8e642b0683bd429b47224de46fbc3
Jake <dominik8655@gmail.com>
2025-01-13 17:58:42 +0100

    feat(admin): add bottom padding to table

--- src/app/(auth)/admin/page.tsx
+++ src/app/(auth)/admin/page.tsx
@@ -4,18 +4,8 @@ import { BackgroundBeams } from "@/components/ui/background-beams";
 import Wrapper from "@/components/wrapper";
 import { env } from "@/env";
 import { getCurrentSession } from "@/server/auth/session";
-import { api, HydrateClient } from "@/trpc/server";
+import { HydrateClient, api } from "@/trpc/server";
 import { redirect } from "next/navigation";
-
-// {post.message
-//   .split("")
-//   .map((char, i) =>
-//     char === "\n" ? (
-//       <br key={i} />
-//     ) : (
-//       <span key={i}>{char}</span>
-//     ),
-//   )}
 
 export default async function AdminDashboard() {
   const { user } = await getCurrentSession();
@@ -29,7 +19,7 @@ export default async function AdminDashboard() {
         </div>
       </div>
       <AdminNavbar />
-      <Wrapper className="pt-16 md:pt-32 lg:pt-48">
+      <Wrapper className="pt-16 pb-16 md:pt-32 lg:pt-48">
         <h1 className="mb-8 text-center font-bold text-4xl md:text-5xl">
           Contact Posts
         </h1>
