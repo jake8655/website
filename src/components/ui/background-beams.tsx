ca81e81b7326118f1d8485f70337f2c7e276121b
Jake <dominik8655@gmail.com>
2025-03-21 22:12:44 +0100

    chore(css): use vars not hardcoded colors

--- src/components/ui/background-beams.tsx
+++ src/components/ui/background-beams.tsx
@@ -136,8 +136,11 @@ const BackgroundBeams = ({ className }: { className?: string }) => {
             gradientUnits="userSpaceOnUse"
             gradientTransform="translate(352 34) rotate(90) scale(555 1560.62)"
           >
-            <stop offset="0.0666667" stopColor="#d4d4d4"></stop>
-            <stop offset="0.243243" stopColor="#d4d4d4"></stop>
+            <stop
+              offset="0.0666667"
+              stopColor="var(--color-neutral-300)"
+            ></stop>
+            <stop offset="0.243243" stopColor="var(--color-neutral-300)"></stop>
             <stop offset="0.43594" stopColor="white" stopOpacity="0"></stop>
           </radialGradient>
         </defs>
